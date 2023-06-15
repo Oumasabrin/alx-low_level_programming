432253f1f81f3f73780a5a8be6098ac6402c5b07
oumasabrin <oumaimaaloui001@gmail.com>
2023-06-15 09:39:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-const char* text="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fputs(text,stdout);	
+const char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fputs(text, stdout);	
 return (1);
 }
