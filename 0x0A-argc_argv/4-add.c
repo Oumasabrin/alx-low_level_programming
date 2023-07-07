cb09a8d60ccdd1b53eeb3193a341bf518a65111a
oumasabrin <oumaimaaloui001@gmail.com>
2023-07-07 09:31:59 -0700

    3-mul

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,68 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include "main.h"
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
+	count = 0;
+	while (count < strlen(str)) /*count string*/
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
 
-    for (int i = 1; i < argc; i++) {
-        char *num = argv[i];
-
-        for (int j = 0; num[j] != '\0'; j++) {
-            if (!isdigit(num[j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
-
-        int numValue = _atoi(num);
-        if (numValue > 0)
-            sum += numValue;
-    }
-
-    printf("%d\n", sum);
-    return 0;
+		count++;
+	}
+	return (1);
 }
 
-int _atoi(char *s) {
-    int i, d, n, len, f, digit;
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
-    i = 0;
-    d = 0;
-    n = 0;
-    len = 0;
-    f = 0;
-    digit = 0;
+int main(int argc, char *argv[])
 
-    while (s[len] != '\0')
-        len++;
+{
 
-    while (i < len && f == 0) {
-        if (s[i] == '-')
-            ++d;
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-        if (s[i] >= '0' && s[i] <= '9') {
-            digit = s[i] - '0';
-            if (d % 2)
-                digit = -digit;
-            n = n * 10 + digit;
-            f = 1;
-            if (s[i + 1] < '0' || s[i + 1] > '9')
-                break;
-            f = 0;
-        }
-        i++;
-    }
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
 
-    if (f == 0)
-        return 0;
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
 
-    return n;
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
 }
 
